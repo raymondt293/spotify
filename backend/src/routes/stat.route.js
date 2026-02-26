5abd9d95ad83e7962aadf301b6d8cbeb77508d74
Raymond Tran <raymondtran483@gmail.com>
2026-02-26 04:02:05 -0800

    User routes and controllers

--- backend/src/routes/stat.route.js
+++ backend/src/routes/stat.route.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
+import { protectedRoute, requireAdmin } from '../middleware/auth.middleware.js';
+import { getStats } from '../controller/stat.controller.js';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send("Stat route with GET method")
-});
+router.get('/', protectedRoute, requireAdmin, getStats);
 
 export default router;
