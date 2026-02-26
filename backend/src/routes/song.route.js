cedf803a9de69c7fb375eecf4774e25016d6750d
Raymond Tran <raymondtran483@gmail.com>
2026-02-26 03:18:42 -0800

    Song routes and controllers

--- backend/src/routes/song.route.js
+++ backend/src/routes/song.route.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import { getAllSongs, getFeaturedSongs, getMadeForYouSongs, getTrendingSongs } from '../controller/song.controller.js';
+import { protectedRoute, requireAdmin } from '../middleware/auth.middleware.js';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send("Song route with GET method")
-});
+router.get('/', protectedRoute, requireAdmin, getAllSongs);
+router.get('/featured', getFeaturedSongs);
+router.get('/made-for-you', getMadeForYouSongs);
+router.get('/trending', getTrendingSongs);
 
 export default router;
